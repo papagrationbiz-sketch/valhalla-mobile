622cf6ccc8511106030003bb66fadfb2d47f546d
Yoshiaki Ishino <aki.gt.vocal@icloud.com>
2026-03-28 20:39:52 +0900

    Final fix: Commit binary XCFramework directly for stable integration

---
+++ build/apple/valhalla-wrapper.xcframework/ios-arm64_x86_64-simulator/Headers/include/main.h
@@ -0,0 +1,32 @@
+#ifndef WRAPPER_H
+#define WRAPPER_H
+
+#include "valhalla_actor.h"
+
+#ifdef __ANDROID__
+#include <jni.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+JNIEXPORT jstring JNICALL Java_com_valhalla_valhalla_ValhallaKotlin_route(JNIEnv *env,
+                                                jobject thiz,
+                                                jstring jRequest,
+                                                jstring jConfigPath);
+
+#ifdef __cplusplus
+}
+#endif
+
+#elif __APPLE__
+
+std::string route(const char *request, void* actor);
+std::string trace_attributes(const char *request, void* actor);
+void* create_valhalla_actor(const char *config_path, ValhallaMobileHttpClient* http_client = nullptr);
+void delete_valhalla_actor(void* actor);
+
+#endif
+
+#endif // WRAPPER_H
