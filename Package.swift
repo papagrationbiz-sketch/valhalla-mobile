2397e2cb667370b84c88f7f766fd17caa6e0553a
papagrationbiz-sketch <253649998+papagrationbiz-sketch@users.noreply.github.com>
2026-08-30 05:49:50 +0000

    Publish Package.swift and bump version to 0.7.0 [skip ci]

--- Package.swift
+++ Package.swift
@@ -5,10 +5,10 @@ import PackageDescription
 let useLocalBinary = false   // ← 手元のファイル（GitHubに直接コミットする実ファイル）を使う設定
 
 // CI will replace the nils with the actual values when building a release
-let version: String = "0.6.0"
+let version: String = "0.7.0"
 let binaryURL: String =
     "https://github.com/papagrationbiz-sketch/valhalla-mobile/releases/download/\(version)/valhalla-wrapper.xcframework.zip"
-let binaryChecksum: String = "88410151f72b068593171d92f2e6f2d983588660a5e8033e516579556012fad7"
+let binaryChecksum: String = "972b443ea4e2e9445cf860b5ae1bf502cf51fe55fc47c0b10ccbd3b889897eed"
 
 // Binary target 設定
 var binaryTarget: Target = .binaryTarget(
